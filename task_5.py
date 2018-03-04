9cafbcf4c2055a961cf83c862bea444ddbbde6f4
Erlend laptop <Erlend.helland@gmail.com>
2018-03-04 16:39:16 +0100

    task 5

--- task_5.py
+++ task_5.py
@@ -8,12 +8,17 @@ from pyspark import SparkConf
 
 sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
 geotweets = sc.textFile("data/geotweets.tsv")
-#records = geotweets.map(lambda x: x.split("\t"))
+records = geotweets.map(lambda x: x.split("\t"))
 sampleGeo = geotweets.sample(False, 0.0005, 5)
 sampleRec = sampleGeo.map(lambda x: x.split("\t"))
 
-countryCode= sampleRec.map(lambda x: (x[2],x[3]))
+#Filtering only relevant columns
+countryCode= records.map(lambda x: (x[2],x[3],x[4]))
+#sorting on US and city tags
+justUS = countryCode.filter(lambda x: x[0]=='US' and x[1]=='city')
+#just place needed for counting
+place= justUS.map(lambda x: (x[2], 1))
+#counting
+counted= place.reduceByKey(lambda x,y: x+y).sortByKey(True,1).sortBy(lambda x: x[1], False)
 
-justUS = countryCode.reduce(lambda x: )
-
-justUS.coalesce(1,True).saveAsTextFile('data/result_5.tsv')
+counted.coalesce(1,True).saveAsTextFile('data/result_5.tsv')
