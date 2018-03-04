869095d5e12b8eeb63b209e555a5378937ba6038
gautesk <gautesk@gmail.com>
2018-03-04 16:48:18 +0100

    Oppdatert kode til task 3 for riktig filtrering, nytt resultat-datasett

--- task_3.py
+++ task_3.py
@@ -4,37 +4,36 @@ findspark.init()
 from pyspark import SparkContext
 from pyspark import SparkConf
 
-def centroid_of_List(latitude, longitude):
-    latitudeCentroid = latitude.sum() / len(latitude)
-    longitudeCentroid = longitude.sum() / len(longitude)
-    return latitudeCentroid, longitudeCentroid
-
 conf = SparkConf().setMaster("local[*]")
 context = SparkContext.getOrCreate(conf)
 geotweets = context.textFile("data/geotweets.tsv", use_unicode=True)
 records = geotweets.map(lambda x: x.split("\t"))
 
-tweetsPerCountry = records.map(lambda x: (x[1], 1)) \
+# Collects a list of all countries with more than 10 tweets
+countriesWithMoreThan10 = records.map(lambda x: (x[1], 1)) \
     .reduceByKey(lambda x,y: x+y) \
     .filter(lambda x: int(x[1]) > 10) \
     .map(lambda x: x[0]) \
     .collect()
 
-tweetsFilteredByCountries = records.filter(lambda x: x[1] in countriesWithMoreThan10) \
-    .map(lambda x: (x[1], x[11]))
-
+# Filter RDD with all tweets to only contain countries with more than 10 tweets
+filteredTweets = records.filter(lambda x: x[1] in countriesWithMoreThan10)
 
 aTuple = (0,0)
-avgLatitude = records.map(lambda x: (x[1], float(x[11]))) \
+# Calculate centroid latitude value of each country
+avgLatitude = filteredTweets.map(lambda x: (x[1], float(x[11]))) \
     .aggregateByKey(aTuple, lambda a,b: (a[0] + b, a[1] + 1),
                             lambda a,b: (a[0] + b[0], a[1] + b[1])) \
                             .mapValues(lambda v: v[0]/v[1])
 
-avgLongitude = records.map(lambda x: (x[1], float(x[12]))) \
+# Calculate centroid longitude value of each country
+avgLongitude = filteredTweets.map(lambda x: (x[1], float(x[12]))) \
     .aggregateByKey(aTuple, lambda a,b: (a[0] + b, a[1] + 1),
                             lambda a,b: (a[0] + b[0], a[1] + b[1])) \
                             .mapValues(lambda v: v[0]/v[1])
 
+# Join longitude and latitude values on key country
 latitudeAndLongitude = avgLatitude.join(avgLongitude)
 
+# Write out results to tsv file
 latitudeAndLongitude.map(lambda x: str(x[0]) + "\t" + str(x[1][0]) + "\t" + str(x[1][1])).coalesce(1,True).saveAsTextFile('data/result_3.tsv')
