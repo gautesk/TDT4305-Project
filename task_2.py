719219830b4f1ea99ce64060defb0334b31ac732
gautesk <gautesk@gmail.com>
2018-03-04 14:03:27 +0100

    Added comments to explain code

--- task_2.py
+++ task_2.py
@@ -9,9 +9,14 @@ context = SparkContext.getOrCreate(conf)
 geotweets = context.textFile("data/geotweets.tsv", use_unicode=True)
 records = geotweets.map(lambda x: x.split("\t"))
 
+# Map our daya to (key, value) pair with (country, 1)
+# Reduce by key (country) to count tweets from each
+# Sort by country first to get alphabetical sort, required for countries with equal tweets in next sort
+# Finally sort by value
 tweetsPerCountry = records.map(lambda x: (x[1], 1)) \
     .reduceByKey(lambda x,y: x+y) \
     .sortByKey(lambda x: x) \
     .sortBy(lambda x: x[1], False)
 
+# Write out results
 tweetsPerCountry.coalesce(1,True).saveAsTextFile('data/result_2')
