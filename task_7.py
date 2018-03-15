d05527cd26527a64ed91d9cebeca4687888a85e3
Erlend laptop <Erlend.helland@gmail.com>
2018-03-15 14:32:57 +0100

    task 7 fix

--- task_7.py
+++ task_7.py
@@ -8,6 +8,7 @@ from pyspark import SparkConf
 
 sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
 geotweets = sc.textFile("data/geotweets.tsv")
+context = SparkContext.getOrCreate(sc)
 records = geotweets.map(lambda x: x.split("\t"))
 sampleGeo = geotweets.sample(False, 0.005, 5)
 sampleRec = sampleGeo.map(lambda x: x.split("\t"))
@@ -21,9 +22,7 @@ place= justUS.map(lambda x: (x[2], 1))
 #counting
 counted= place.reduceByKey(lambda x,y: x+y).sortByKey(True,1).sortBy(lambda x: x[1], False).take(5)
 
-cities=[]
-for line in counted:
-    cities.append(line[0])
+citiesRDD=context.parallelize(counted)
 
 stopWords = []
 with open('data/stop_words.txt', 'r') as stopwords:
@@ -38,16 +37,15 @@ stopwords.close()
 # Map and reduce to count words on the form (word, wordcount)
 # Sort words by count value, take top 10 words
 
-usTweets = records.filter(lambda x: x[4] in cities) \
-                .flatMap(lambda x: ((x[4],y) for y in x[10].lower().split(" "))) \
-                .filter(lambda x: x[1] not in stopWords and len(x[1]) > 2) \
-                .map(lambda x: ((x[0], x[1]),1)) \
+usTweets = records.map(lambda x: (x[4],x[10])) \
+                .join(citiesRDD).sortBy(lambda x: x[1][1], False) \
+                .map(lambda x: ((x[0],x[1][0]),x[1][1])) \
+                .flatMap(lambda x: (((x[1],x[0]),y) for y in x[0][1].lower().split(" "))) \
+                .filter(lambda x: x[1] not in stopWords and len(x[1]) > 1) \
+                .map(lambda x: (((x[0][0],x[0][1][0],x[1]),1))) \
                 .reduceByKey(lambda x,y: x+y) \
-                .map(lambda x: ((x[0][0]),(x[0][1],x[1]))).sortBy(lambda x: x[1][1], False) \
-                .groupByKey().mapValues(list) \
-                .map(lambda x: (x[0],x[1][:10]))
+                .map(lambda x: (((x[0][0],x[0][1])),(x[0][2],x[1]))).sortBy(lambda x: x[1][1], False) \
+                .groupByKey().mapValues(list).sortBy(lambda x: x[0][0], False) \
+                .map(lambda x: (x[0][1],x[1][:10]))
 
-
-
-
-usTweets.coalesce(1,True).saveAsTextFile('data/result_7.tsv')
+usTweets.coalesce(1,True).saveAsTextFile('data/result_8.tsv')
